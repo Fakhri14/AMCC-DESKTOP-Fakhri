67b0e2effb7417fabe916277d7e13b236f3eea92
Fakhri <fakhriabdullahtanjung88@gmail.com>
2019-12-21 17:16:22 +0900

    Dictionary Python

---
+++ pertemuan4.py
@@ -0,0 +1,36 @@
+# dict ={'MNG':'5KG', 'JRK':'10KG', 'HMNG':'RP 10.000', 'HJRK':'RP 5.000'}
+
+# print ("Andi Membeli Mangga", dict['MNG'], ", Jeruk", dict['JRK'])
+# print ("Harga Mangga ", dict['MNG'])
+# print ("Harga Jeruk ", dict['JRK']
+
+# def luas_segitiga(alas, tinggi):
+#     luas = (alas * tinggi) / 2
+#     print ("Luas Segitiga : %f"%luas)
+
+# luas_segitiga(10, 65)
+
+# def luas_persegi(sisi):
+#     return (sisi*sisi)
+
+# print (luas_persegi(4))
+
+# def luas_segitiga(alas,tinggi):
+#     return  (alas*tinggi)/2
+
+# print('Luas Segitiga %d'%luas_segitiga(6,8))
+
+nama = "Salsa"
+versi = "1.0.0"
+
+def help():
+    nama = "Programku"
+    versi = "1.0.2"
+
+    print("Nama: %s"%nama)
+    print("Versi: %s"%versi)
+
+print ("Nama: %s"%nama)
+print ("Versi: %s"%versi)
+
+help()
