22d99c262121972bcd1fef0b125e6149042b8df1
Fakhri <fakhriabdullahtanjung88@gmail.com>
2020-01-04 18:22:20 +0900

    review materi

---
+++ per5.py
@@ -0,0 +1,15 @@
+# def km(a):
+#     return a/1000
+
+# jarak = int(input('Masukan Nilai dalam km: '))
+
+# print('jarak dalam km: ', km(jarak))
+
+# from math import sqrt
+
+#  help(sqrt)
+
+# b = input("Masukan nilai: ")
+# a = sqrt(int(b))
+
+print("nilai
